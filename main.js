53f594b0fe878ed41955936fd32e4be1896492d3
Timber <Macroch@yandex.ru>
2018-06-01 00:40:51 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.slider__items').slick({
+        appendArrows: '.slider__btn',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>'
+    });
+});
