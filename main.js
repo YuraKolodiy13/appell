3bdacf8f06fb91456a351a522fc6f688c0f47fef
Timber <Macroch@yandex.ru>
2018-06-01 21:26:07 +0300

    edit style

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 $(function () {
     $('.slider__items').slick({
         appendArrows: '.slider__btn',
-        nextArrow: '<button type="button" class="slick-next slick-next-img"><i class="fas fa-chevron-right"></i></button>',
-        prevArrow: '<button type="button" class="slick-prev slick-prev-img"><i class="fas fa-chevron-left"></i></button>'
+        nextArrow: '<button type="button" class="slick-next slick-next-img"></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-prev-img"</button>'
     });
     $('.slider__title').slick({
         appendArrows: '.slick-prev-img',
         vertical: true,
-        nextArrow: '<button type="button" class="slick-next slick-next-text"><i class="fas fa-chevron-right"></i></button>',
-        prevArrow: '<button type="button" class="slick-prev slick-prev-text"><i class="fas fa-chevron-left"></i></button>'
+        nextArrow: '<button type="button" class="slick-next slick-next-text"></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-prev-text"</button>'
     });
     $('.slider__text').slick({
         appendArrows: '.slick-prev-text',
         vertical: true,
-        nextArrow: '<button type="button" class="slick-next slick-next-title"><i class="fas fa-chevron-right"></i></button>',
-        prevArrow: '<button type="button" class="slick-prev slick-prev-title"><i class="fas fa-chevron-left"></i></button>'
+        nextArrow: '<button type="button" class="slick-next slick-next-title"></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-prev-title"</button>'
     });
 
     document.querySelector('.slick-next-img').appendChild(document.querySelector('.slick-next-text'));
